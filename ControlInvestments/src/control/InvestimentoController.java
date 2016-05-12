9d4154f7eda306d157487f25437a82acbd2342dd
raiffsantos <raiff_santos@hotmail.com>
2016-05-12 01:32:46 -0300

    Criando controlador da classe Investimento e testes unitários de InvestidorController

---
+++ ControlInvestments/src/control/InvestimentoController.java
@@ -0,0 +1,33 @@
+
+package control;
+
+import java.util.ArrayList;
+import model.Investimento;
+
+/**
+ *
+ * @author Raiff
+ */
+public class InvestimentoController {
+    
+    public static boolean cadastrarInvestimento(){
+        return false;
+    }
+    
+    public static boolean deletarInvestimento(){
+        return false;
+    }
+    
+    public static boolean editarInvestimento(){
+        return false;
+    }
+    
+    public static ArrayList<Investimento> listarInvestimentos(){
+        ArrayList<Investimento> investimentos = new ArrayList<Investimento>();
+        return investimentos;
+    }
+    
+    public static boolean investir(){
+        return false;
+    }
+}
